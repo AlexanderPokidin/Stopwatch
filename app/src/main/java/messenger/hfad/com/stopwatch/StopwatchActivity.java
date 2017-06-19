0a60709fd6646740a4a6ba2bcbfc482d63ba15c8
AlexanderPokidin <a.pokidin@gmail.com>
2017-06-19 17:27:12 +0300

    Stopwatch is done

--- app/src/main/java/messenger/hfad/com/stopwatch/StopwatchActivity.java
+++ app/src/main/java/messenger/hfad/com/stopwatch/StopwatchActivity.java
@@ -2,7 +2,9 @@ package messenger.hfad.com.stopwatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
 
 public class StopwatchActivity extends Activity {
 
@@ -13,6 +15,7 @@ public class StopwatchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
     }
 
     public void onClickStart(View view){
@@ -26,5 +29,25 @@ public class StopwatchActivity extends Activity {
     public void onClickReset(View view){
         running = false;
         second = 0;
+    }
+
+    private void runTimer(){
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = second/3600;
+                int minutes = (second%3600)/60;
+                int secs = second%60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+
+                if (running){
+                    second++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
